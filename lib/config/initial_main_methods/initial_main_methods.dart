77c7cfba862fd667a282d269e8eb9926bcc1c00e
Zakaria AIDOUNI <zakariaaidouni@gmail.com>
2024-06-26 16:35:53 +0200

    add .env

--- lib/config/initial_main_methods/initial_main_methods.dart
+++ lib/config/initial_main_methods/initial_main_methods.dart
@@ -1,13 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '../../firebase_options.dart';
 
 class MainMethods {
   static Future<void> init() async {
+
     // init binding
     WidgetsFlutterBinding.ensureInitialized();
+
+     // Load dotenv variables
+    await dotenv.load();
 
     // init firebase
     await Firebase.initializeApp(
